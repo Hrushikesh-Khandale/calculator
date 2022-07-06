7089ba57dd15b2cebf10bdf5bd818c7982bde58e
Hrushikesh Khandale <khandalehrushikesh@gmail.com>
2022-07-06 21:13:10 +0530

    css added

--- src/components/NumberKeys.js
+++ src/components/NumberKeys.js
@@ -1,47 +1,68 @@
-import { clear } from "@testing-library/user-event/dist/clear";
 import React, { useEffect, useRef, useState } from "react";
 
 const NumberKeys = () => {
-  // eslint-disable-next-line
   const inputRef = useRef(null);
   const [data, setData] = useState("");
 
-  useEffect(()=>{inputRef.current.focus()})
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
-const backSpace=()=>{
-  setData(data.slice(0, -1))
-}  
+  const backSpace = () => {
+    setData(data.slice(0, -1));
+  };
 
-  const handleClick=(e)=>{
-    setData(data.concat(e.target.value))
-  }
+  const handleClick = (e) => {
+    setData(data.concat(e.target.value));
+  };
+
   const clear = () => {
     setData("");
     console.log(data);
   };
 
   const handleResult = (e) => {
-    // e.preventDefault();
+    // eslint-disable-next-line
     setData(eval(data).toString());
+    // eslint-disable-next-line
     console.log(eval(data));
+    // e.preventDefault();
   };
 
   return (
-    <>
+    <div className="container" style={{border:'2px solid black',borderRadius:'10px',paddingTop:'40px',height:'22pc',width:'25pc',marginLeft:'40%',background:'#ff7979' }}>
       <input
         type="text"
         readOnly
         ref={inputRef}
         value={data}
-        style={{ height: "3pc", width: "20pc", color: "black" }}
+        style={{
+          height: "3pc",
+          width: "20pc",
+          background: "white",
+          color: "black",
+          marginBottom: "1pc",
+          fontSize: "1.5rem",
+          fontFamily: "Consolas",
+          border:'10px groove smokewhite'
+        }}
       />
       <div
         className="btn-toolbar"
+        id="buttons"
         role="toolbar"
         aria-label="Toolbar with button groups"
       >
         <div className="btn-group me-2" role="group" aria-label="First group">
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="1"
             type="button"
@@ -50,6 +71,14 @@ const backSpace=()=>{
             1
           </button>
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="2"
             type="button"
@@ -58,6 +87,14 @@ const backSpace=()=>{
             2
           </button>
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="3"
             type="button"
@@ -66,6 +103,14 @@ const backSpace=()=>{
             3
           </button>
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="+"
             type="button"
@@ -73,12 +118,32 @@ const backSpace=()=>{
           >
             +
           </button>
-          <button onClick={clear} type="button" className="btn btn-primary">
+          <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
+            onClick={clear}
+            type="button"
+            className="btn btn-primary"
+          >
             Clear
           </button>
         </div>
         <div className="btn-group me-2" role="group" aria-label="Second group">
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="4"
             type="button"
@@ -87,6 +152,14 @@ const backSpace=()=>{
             4
           </button>
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="5"
             type="button"
@@ -95,6 +168,14 @@ const backSpace=()=>{
             5
           </button>
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="6"
             type="button"
@@ -103,6 +184,14 @@ const backSpace=()=>{
             6
           </button>
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="-"
             type="button"
@@ -111,6 +200,14 @@ const backSpace=()=>{
             -
           </button>
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={backSpace}
             value="-"
             type="button"
@@ -121,6 +218,14 @@ const backSpace=()=>{
         </div>
         <div className="btn-group" role="group" aria-label="Third group">
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="7"
             type="button"
@@ -129,6 +234,14 @@ const backSpace=()=>{
             7
           </button>
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="8"
             type="button"
@@ -137,6 +250,14 @@ const backSpace=()=>{
             8
           </button>
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="9"
             type="button"
@@ -145,6 +266,14 @@ const backSpace=()=>{
             9
           </button>
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="*"
             type="button"
@@ -152,9 +281,33 @@ const backSpace=()=>{
           >
             *
           </button>
+          <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
+            onClick={handleClick}
+            value="/"
+            type="button"
+            className="btn btn-primary"
+          >
+            /
+          </button>
         </div>
         <div className="btn-group" role="group" aria-label="Fourth group">
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="."
             type="button"
@@ -163,6 +316,14 @@ const backSpace=()=>{
             .
           </button>
           <button
+            style={{
+              height: "3pc",
+              width: "3pc",
+              margin: " 5px 11px",
+              background: "#ff3e3e",
+              color: "white",
+              fontSize: "1rem",
+            }}
             onClick={handleClick}
             value="0"
             type="button"
@@ -171,6 +332,13 @@ const backSpace=()=>{
             0
           </button>
           <button
+            style={{
+              height: "3pc",
+              width: "11.8pc",
+              margin: " 5px 11px",
+              background: "#bf1f1f",
+              fontSize: "1rem",
+            }}
             onClick={handleResult}
             type="button"
             className="btn btn-primary"
@@ -179,7 +347,7 @@ const backSpace=()=>{
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
